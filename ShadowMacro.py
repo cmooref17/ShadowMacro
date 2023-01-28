7ba8f8b7f10bc7c3c73d0d6fa04cd0e871f167d5
cmooref17 <cmooref17@gmail.com>
2023-01-27 21:32:17 -0700

    I forgot

--- ShadowMacro.py
+++ ShadowMacro.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import json
 
 from pynput import keyboard
 from pynput import mouse
@@ -51,6 +52,12 @@ macro_speed = 1.0
 loop_macro = False
 D_SPEED_MACRO = 0.5
 
+macro_start_time = 0
+
+#mouse.Button.
+
+#d = keyboard.Key.
+
 
 def stop_script():
     exit()
@@ -58,21 +65,26 @@ def stop_script():
 def on_key_press(key):
     global key_states, key_states_recording, recording, stop_macro, loop_macro
     
-    k = None
     try:
         k = key.char
     except AttributeError:
-        k = key
+        k = key.name
     
     if (k in key_states.keys() and key_states[k] == 1):
         return
     
     key_states[k] = 1
-    
-    
-    if ((keyboard.Key.ctrl_l in key_states and key_states[keyboard.Key.ctrl_l] == 1) and (keyboard.Key.shift in key_states and key_states[keyboard.Key.shift] == 1)):
+    if (("ctrl_l" in key_states and key_states["ctrl_l"] == 1) and ("shift" in key_states and key_states["shift"] == 1)):
         
-        if (k == f9):
+        if (k == "f5"):
+            save_macro()
+            return
+        
+        elif (k == "f6"):
+            load_macro()
+            return
+        
+        elif (k == "f9"):
             if (running_macro):
                 print("Cannot record macro while a macro is running.")
                 play_sound(cant_do_that_macro_audio_path)
@@ -81,17 +93,17 @@ def on_key_press(key):
             else:
                 # don't record hotkeys that stopped the recording
                 for action in reversed(recording):
-                    if (action[1] == "key_press" and action[2] == keyboard.Key.ctrl_l):
+                    if (action[1] == "key_press" and action[2] == "ctrl_l"):
                         recording.remove(action)
                         break
                 for action in reversed(recording):
-                    if (action[1] == "key_press" and action[2] == keyboard.Key.shift):
+                    if (action[1] == "key_press" and action[2] == "shift"):
                         recording.remove(action)
                         break
                 stop_recording()
             return
         
-        if (k == f10 or k == f8):
+        elif (k == "f8" or k == "f10"):
             if (is_recording):
                 print("Cannot play macro while recording.")
                 play_sound(cant_do_that_macro_audio_path)
@@ -99,14 +111,14 @@ def on_key_press(key):
                 print("Must record macro before running it.")
                 play_sound(cant_do_that_macro_audio_path)
             elif (not running_macro):
-                loop_macro = (True, False)[k == f10]
+                loop_macro = (True, False)[k == "f10"]
                 recording_thread = threading.Thread(target = run_macro)
                 recording_thread.start()
             else:
                 stop_macro = True
             return
         
-        if (k == f11):
+        elif (k == "f11"):
             if (running_macro):
                 print("Cannot change macro speed while it's running.")
                 play_sound(cant_do_that_macro_audio_path)
@@ -115,7 +127,7 @@ def on_key_press(key):
                 play_sound(cant_do_that_macro_audio_path)
             else:
                 slow_down_macro()
-        if (k == f12):
+        elif (k == "f12"):
             if (running_macro):
                 print("Cannot change macro speed while it's running.")
                 play_sound(cant_do_that_macro_audio_path)
@@ -132,17 +144,16 @@ def on_key_press(key):
     
     current_time = time.time()
     key_states_recording[k] = 1
-    recording.append([current_time, "key_press", k, 1])
+    recording.append([current_time - macro_start_time, "key_press", k, 1])
     
     
 def on_key_release(key):
     global key_states, key_states_recording, recording
     
-    k = None
     try:
         k = key.char
     except AttributeError:
-        k = key
+        k = key.name
     
     if (not k in key_states.keys() or key_states[k] == 0):
         return
@@ -156,7 +167,7 @@ def on_key_release(key):
     
     current_time = time.time()
     key_states_recording[k] = 0
-    recording.append([current_time, "key_press", k, 0])
+    recording.append([current_time - macro_start_time, "key_press", k, 0])
 
 
 def on_mouse_move(x, y):
@@ -166,25 +177,26 @@ def on_mouse_move(x, y):
         return
     
     current_time = time.time()
-    recording.append([current_time, "move_mouse", x, y])
+    recording.append([current_time - macro_start_time, "move_mouse", x, y])
 
 
 def on_mouse_click(x, y, button, pressed):
     global recording, mouse_states, mouse_states_recording
     
-    mouse_states[button.name] = pressed
+    b = button.name
+    mouse_states[b] = pressed
     
     if (not is_recording):
         return
     
-    if (pressed and button.name in mouse_states_recording.keys() and mouse_states_recording[button.name] == pressed):
+    if (pressed and b in mouse_states_recording.keys() and mouse_states_recording[b] == pressed):
         return
-    if (not pressed and (not button.name in mouse_states_recording.keys() or mouse_states_recording[button.name] == pressed)):
+    if (not pressed and (not b in mouse_states_recording.keys() or mouse_states_recording[b] == pressed)):
         return
     
     current_time = time.time()
-    mouse_states_recording[button.name] = pressed
-    recording.append([current_time, "mouse_click", button, pressed, x, y])
+    mouse_states_recording[b] = pressed
+    recording.append([current_time - macro_start_time, "mouse_click", b, pressed, x, y])
 
 
 def on_mouse_scroll(x, y, dx, dy):
@@ -194,26 +206,27 @@ def on_mouse_scroll(x, y, dx, dy):
         return
     
     current_time = time.time()
-    recording.append([current_time, "mouse_scroll", dx, dy, x, y])
+    recording.append([current_time - macro_start_time, "mouse_scroll", dx, dy, x, y])
 
 
 
 
 def start_recording():
-    global is_recording, recording
+    global is_recording, recording, macro_start_time
     
-    keyboard_controller.release(keyboard.Key.ctrl_l)
-    keyboard_controller.release(keyboard.Key.shift)
-    keyboard_controller.release(keyboard.Key.f9)
-    mouse_states[keyboard.Key.ctrl_l] = 0
-    mouse_states[keyboard.Key.shift] = 0
-    mouse_states[keyboard.Key.f9] = 0
+    keyboard_controller.release(keyboard.Key["ctrl_l"])
+    keyboard_controller.release(keyboard.Key["shift"])
+    keyboard_controller.release(keyboard.Key["f9"])
+    mouse_states["ctrl_l"] = 0
+    mouse_states["shift"] = 0
+    mouse_states["f9"] = 0
     
     if (is_recording):
         return
     
     play_sound(start_record_audio_path)
     print("RECORDING STARTED")
+    macro_start_time = time.time()
     play_sound(start_record_audio_path)
     is_recording = True
     recording = []
@@ -254,12 +267,12 @@ def slow_down_macro(amount = D_SPEED_MACRO):
 def run_macro():
     global is_recording, recording, running_macro, stop_macro, loop_macro
     
-    keyboard_controller.release(keyboard.Key.ctrl_l)
-    keyboard_controller.release(keyboard.Key.shift)
-    keyboard_controller.release(keyboard.Key.f10)
-    mouse_states[keyboard.Key.ctrl_l] = 0
-    mouse_states[keyboard.Key.shift] = 0
-    mouse_states[keyboard.Key.f10] = 0
+    keyboard_controller.release(keyboard.Key["ctrl_l"])
+    keyboard_controller.release(keyboard.Key["shift"])
+    keyboard_controller.release(keyboard.Key["f10"])
+    mouse_states["ctrl_l"] = 0
+    mouse_states["shift"] = 0
+    mouse_states["f10"] = 0
     
     pressed_keys = []
     pressed_buttons = []
@@ -275,7 +288,6 @@ def run_macro():
     while(not (played_once) or loop_macro):
         played_once = True
         start_time = time.time()
-        macro_start_time = recording[0][0]
         macro_time = 0
         
         action_num = 0
@@ -283,7 +295,7 @@ def run_macro():
             current_time = time.time()
             macro_time = (current_time - start_time)
             action_time = action[0]
-            action_time_rel = (action_time - macro_start_time) / macro_speed
+            action_time_rel = action_time / macro_speed
             
             # wait until execution
             wait_time = action_time_rel - macro_time
@@ -294,32 +306,38 @@ def run_macro():
                 mouse_controller.position = (action[2], action[3])
                 
             elif (action[1] == "mouse_click"):
-                button = action[2]
+                button_name = action[2]
                 pressed = action[3]
                 mouse_controller.position = (action[4], action[5])
                 time.sleep(0.01)
                 if (pressed):
-                    mouse_controller.press(button)
-                    if (not button in pressed_buttons):
-                        pressed_buttons.append(button)
+                    mouse_controller.press(mouse.Button[button_name])
+                    if (not button_name in pressed_buttons):
+                        pressed_buttons.append(button_name)
                 else:
-                    mouse_controller.release(button)
-                    if (button in pressed_buttons):
-                        pressed_buttons.remove(button)
+                    mouse_controller.release(mouse.Button[button_name])
+                    if (button_name in pressed_buttons):
+                        pressed_buttons.remove(button_name)
                 time.sleep(0.01)
                 
             elif (action[1] == "key_press"):
-                key = action[2]
+                key_name = action[2]
                 pressed = action[3]
                 time.sleep(0.01)
                 if (pressed):
-                    keyboard_controller.press(key)
-                    if (not key in pressed_keys):
-                        pressed_keys.append(key)
+                    if (len(key_name) == 1):
+                        keyboard_controller.press(key_name)
+                    else:
+                        keyboard_controller.press(keyboard.Key[key_name])
+                    if (not key_name in pressed_keys):
+                        pressed_keys.append(key_name)
                 else:
-                    keyboard_controller.release(key)
-                    if (key in pressed_keys):
-                        pressed_keys.remove(key)
+                    if (len(key_name) == 1):
+                        keyboard_controller.release(key_name)
+                    else:
+                        keyboard_controller.release(keyboard.Key[key_name])
+                    if (key_name in pressed_keys):
+                        pressed_keys.remove(key_name)
                 time.sleep(0.01)
                     
             elif (action[1] == "mouse_scroll"):
@@ -335,10 +353,10 @@ def run_macro():
                 break
     
     # release keys/buttons so they don't stay pressed
-    for key in pressed_keys:
-        keyboard_controller.release(key)
-    for button in pressed_buttons:
-        mouse_controller.release(button)
+    for key_name in pressed_keys:
+        keyboard_controller.release(keyboard.Key[key_name])
+    for button_name in pressed_buttons:
+        mouse_controller.release(mouse.Button[button_name])
     
     running_macro = False
     loop_macro = False
@@ -350,6 +368,23 @@ def run_macro():
         print("END MACRO")
         play_sound(end_macro_audio_path)
 
+
+def save_macro(filename = "filename.json"):
+    if (is_recording or running_macro):
+        return
+    with open(filename, "w") as file:
+        json.dump(recording, file, indent = 4)
+    print(f"Saved macro to file: {filename}")
+
+
+def load_macro(filename = "filename.json"):
+    global recording
+    if (is_recording or running_macro):
+        return
+    with open(filename, "r") as file:
+        recording = json.load(file)
+    print(f"Loaded macro from file: {filename}")
+    
 
 def play_sound(path):
     recording_thread = threading.Thread(target=playsound, args=(path,), daemon=True)
@@ -371,7 +406,6 @@ def main():
     mouse_listener.join()
     
     
-
 
 
 
